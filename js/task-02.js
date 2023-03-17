14b50ed158dbe1b160b06f432248ed5fa633dabe
petekostrytsya <petekostrytsya@gmail.com>
2023-03-17 22:39:29 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,12 +8,12 @@ const ingredients = [
 ];
 
 
-
 const list = document.querySelector('#ingredients');
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  let item = document.createElement('li');
-  item.textContent = ingredients[i];
-  item.classList.add('item');
-  list.appendChild(item);
+for (let i = 0; i < ingredients.length; i++) {
+  let item = document.createElement("li");
+  item.innerText = ingredients[i];
+  item.classList.add("item");
+  list.append(item);
 }
+
